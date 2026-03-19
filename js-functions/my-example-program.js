07a6f4f5f6aaab156e9c8a9b1ca66e7cade23026
jolynestarchaser <mrkaokungkub@gmail.com>
2026-03-19 14:25:04 +0700

    add javascript function and object

---
+++ js-functions/my-example-program.js
@@ -0,0 +1,15 @@
+function greet(name, role2) {
+  // write fuction logic here
+  const role = "Coder";
+  // set return value of this function
+  return `Hello, my name ${name}. I work as ${role} but at night I am a ${role2}.`;
+}
+// 'function' keywork
+// function name
+// bracket () syntax
+// bracket {} syntax
+// 'return' keywork
+
+greet();
+
+console.log(greet("lnwza", "hahabuak"));
